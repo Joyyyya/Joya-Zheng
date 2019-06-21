57fa0a981a0e96b5e5229b4d9d4dda95975724ea
Joya Zheng <Joya@zhengdeMacBook-Air.local>
2019-06-21 23:07:18 +0800

    first change

---
+++ src/pages/p2.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import './p2.css'
+
+
+
+const p2 = () => (
+  <div className="content">
+    <h1>Hi from the second page</h1>
+    <p>Welcome to p2</p>
+    <Link to="/">Go back to the homepage</Link>
+    
+  </div>
+)
+
+export default p2
