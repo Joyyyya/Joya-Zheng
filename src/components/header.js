9e36671c55ca923c4c94c4b6fbeb9dc8ce118597
Jolene Zheng <Jolenezzj@gmail.com>
2019-09-04 17:42:18 +0800

    更改简历地址

--- src/components/header.js
+++ src/components/header.js
@@ -28,7 +28,7 @@ import './Header.css'
          <Link to="/">                       </Link>
          <Link to="/">                       </Link>
          <a href="/#Card"><span className="header-nav-link">项目</span></a>
-         <a href="https://join.qq.com/upload/resume_file/190901/9b5fe90979a97ec5fed3757b87c476de.pdf" target="_blank"><span className="header-nav-link">简历</span></a>
+         <a href="https://join.qq.com/upload/resume_file/190904/28a9df7f89bc1768ae4f160efa7ab35e.pdf" target="_blank"><span className="header-nav-link">简历</span></a>
          <a href="http://www.woshipm.com/u/823776" target="_blank"><span className="header-nav-link">文章</span></a>
        </div>
      </div> 
